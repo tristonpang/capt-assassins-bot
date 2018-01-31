f1f80f0d7a7e2c1e4f9b5c6ea5d8821de4914fdc
Yen Ter <leeyenter@gmail.com>
2018-01-31 15:43:36 +0800

    Add testing code for telegram

--- assassins_web.py
+++ assassins_web.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect, Response, jsonify
 import psycopg2
-
 from werkzeug.contrib.cache import SimpleCache
+from telegram import teleBot
 
 cache = SimpleCache()
 
