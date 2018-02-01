6146ed349c898b7ef8ac9e29847fcf01e12741c3
Yen Ter <leeyenter@gmail.com>
2018-02-01 12:41:03 +0800

    Pass user_alive to template

--- assassins_web.py
+++ assassins_web.py
@@ -41,7 +41,7 @@ def displayPage(token):
         target_name = task_data[1]
 
     #slice data, add into return statement
-    return render_template("player-info.html", token = token, user_nick = user_nickname, user_name = user_name, task = task_desc, target = target_name)
+    return render_template("player-info.html", token = token, user_alive = user_alive, user_nick = user_nickname, user_name = user_name, task = task_desc, target = target_name)
 
 @app.route("/assassins/<token>/kill")
 def killPage(token):
