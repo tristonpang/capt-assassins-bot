407b3f60ae9fc51d3f352732274f8cbe6a2b4581
Yen Ter <leeyenter@gmail.com>
2018-02-01 23:33:25 +0800

    Add admin endpoint

--- assassins_web.py
+++ assassins_web.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from telegram import teleBot
 from users_endpoints import usersEndpoints
+from admin_endpoints import adminEndpoints
 
 app = Flask(__name__)
 app.register_blueprint(usersEndpoints)
+app.register_blueprint(adminEndpoints)
 app.register_blueprint(teleBot)
 
 if __name__ == "__main__":
