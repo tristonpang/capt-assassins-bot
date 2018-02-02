fb2fe3e5d4c5367c95bb5a4e0ed8157aff71eaa3
Yilun <yilun95@gmail.com>
2018-02-02 21:56:19 +0800

    admin page style update

--- admin_endpoints.py
+++ admin_endpoints.py
@@ -9,34 +9,34 @@ adminEndpoints = Blueprint('adminEndpoints', __name__, template_folder='template
 
 @adminEndpoints.route("/assassins/admin")
 def adminIndex():
-	cur = conn.cursor()
-	cur.execute("SELECT u1.user_name AS Player, u1.user_nickname AS Nickname, t.task_description AS task, u2.user_name AS Target FROM users u1, users u2, tasks t, contracts c WHERE u1.user_id = c.contract_assid AND u2.user_id = contract_targetid AND c.contract_taskid = t.task_id")
-	data = cur.fetchall()
-	return render_template("admin-info.html", data=data)
+    cur = conn.cursor()
+    cur.execute("SELECT u1.user_name AS Player, u1.user_nickname AS Nickname, t.task_description AS task, u2.user_name AS Target FROM users u1, users u2, tasks t, contracts c WHERE u1.user_id = c.contract_assid AND u2.user_id = contract_targetid AND c.contract_taskid = t.task_id")
+    data = cur.fetchall()
+    return render_template("admin-info.html", data=data)
 
 
 @adminEndpoints.route("/assassins/addplayer")
 def displayAdmin():
-	return render_template("admin-add.html")
+    return render_template("admin-add.html")
 
 @adminEndpoints.route("/assassins/deleteplayer")
 def displayDelete():
-	return render_template("admin-delete.html")
+    return render_template("admin-delete.html")
 
 
 @adminEndpoints.route("/assassins/admin/addplayersubmit", methods=['POST'])
 def displaySubmit():
-	cur = conn.cursor()
-	print(request.form)
-	cur.execute("INSERT into users (user_nickname, user_name, user_password) VALUES (%s, %s, %s)",
-				(request.form['nickname'], request.form['name'], request.form['token']))
-	conn.commit()
-	return render_template("admin-success.html")
+    cur = conn.cursor()
+    print(request.form)
+    cur.execute("INSERT into users (user_nickname, user_name, user_password) VALUES (%s, %s, %s)",
+                (request.form['nickname'], request.form['name'], request.form['token']))
+    conn.commit()
+    return render_template("admin-success.html")
 
 @adminEndpoints.route("/assassins/admin/deleteplayersubmit", methods=['POST'])
 def displayDelSuccess():
-	cur = conn.cursor()
-	print(request.form)
-	cur.execute("DELETE FROM users WHERE user_password = %s", (request.form['exampleInputToken']))
-	conn.commit()
-	return render_template("admin-deletesuccess.html")
+    cur = conn.cursor()
+    print(request.form)
+    cur.execute("DELETE FROM users WHERE user_password = %s", (request.form['exampleInputToken']))
+    conn.commit()
+    return render_template("admin-deletesuccess.html")
