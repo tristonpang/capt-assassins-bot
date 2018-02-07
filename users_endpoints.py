73016255589c5b750764273fc9bb2ae95f9b466b
Yen Ter <leeyenter@gmail.com>
2018-02-07 11:36:36 +0800

    Notify all players on assassination

--- users_endpoints.py
+++ users_endpoints.py
@@ -4,7 +4,7 @@ import psycopg2
 from datetime import datetime
 from private_vars import telegramBotURL, connStr
 import random
-from telegram import sendMsg
+from telegram import sendMsg, fetchStatus
 
 # from flask_cache import Cache
 
@@ -68,35 +68,37 @@ def killPage(token):
     conn = psycopg2.connect(connStr)
     conn.autocommit = True
     cur = conn.cursor()
-    cur.execute("SELECT users.user_id, contracts.contract_id, contract_targetID, users.user_nickname \
-        FROM users INNER JOIN contracts ON users.user_id = contracts.contract_assId \
+    cur.execute("SELECT users.user_id, contracts.contract_id, contract_targetID, users.user_nickname, \
+        users.user_telegram FROM users INNER JOIN contracts ON users.user_id = contracts.contract_assId \
         WHERE contracts.contract_complete is null and users.user_password = %s", (token,))
     data = cur.fetchone()
     user_id = data[0]
     contract_id = data[1]
     target_id = data[2]
     user_nickname = data[3]
+    user_telegram = data[4]
+
     cur.execute("UPDATE users SET user_alive = FALSE WHERE user_id = %s", (target_id,))
-    # conn.commit()
     cur.execute("UPDATE contracts SET contract_complete = now() WHERE contract_id = %s", (contract_id,))
-    # conn.commit()
     cur.execute("UPDATE contracts SET contract_assID = %s WHERE contract_assID = %s", (user_id, target_id))
-    # conn.commit()
     #set target's status to dead
     #set user's new target and task
-    cur.execute("SELECT user_name, user_telegram FROM users WHERE user_id = %s", (target_id,))
-    data = cur.fetchone()
-    old_target_name = data[0]
-    target_chat_id = data[1]
+    # cur.execute("SELECT user_name, user_telegram FROM users WHERE user_id = %s", (target_id,))
+    # data = cur.fetchone()
+    # old_target_name = data[0]
+    # target_chat_id = data[1]
 
-    #inform target that he/she is dead
-    # cur.execute("SELECT user_telegram FROM users WHERE user_id = %s", (target_id,))
-    # target_chat_id = cur.fetchone()
-    if target_chat_id != None:
-        message = "*Oh no!* You have been killed by _" + user_nickname + "_!"
-        sendMsg(target_chat_id, message)
+    currStatus = fetchStatus(cur) # get the current status
+    cur.execute("SELECT user_id, user_name, user_telegram FROM users WHERE user_telegram IS NOT NULL")
+    telegramIDs = cur.fetchall()
+
+    for teleUser in telegramIDs:
+        if teleUser[0] == target_id:
+            sendMsg(teleUser[1], "*Oh no!* You have been killed by _" + user_nickname + "_!")
+        else:
+            sendMsg(teleUser[1], "_There has been an assassination._")
+        sendMsg(teleUser[1], currStatus)
     
     cur.close()
 
-    # return render_template("player-killconfirmed.html", dead_target = old_target_name)
-    return redirect("/assassins/" + token + "/?msg=You+have+assassinated+"+old_target_name+".")
+    return redirect("/assassins/" + token + "/?msg=You+have+assassinated+your+target.")
