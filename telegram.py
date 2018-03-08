87e406eb03bc67510f2159f4b0c64fcd61a1c768
Yen Ter (Chromebook) <leeyenter@gmail.com>
2018-03-08 14:26:05 +0800

    Update stylistic

--- telegram.py
+++ telegram.py
@@ -97,10 +97,10 @@ def fetchStatus(cur):
         #     userData += "1 kill"
         # userData += ")"
         if user[1]:
-            outputStr += "`" + user[0] + "` " + kills + "\n"
+            outputStr += user[0] + kills + "\n"
         else:
             if alive:
                 alive = False
                 outputStr += "\n*Dead Players*\n"
-            outputStr += user[0] + kills + " - dead\n"
+            outputStr += user[0] + kills + "\n"
     return outputStr
